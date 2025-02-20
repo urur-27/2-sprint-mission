3c6e59d8d1cfffe4150628b39a4ea8e1165ebae2
김민식 <u1r2r3@naver.com>
2025-02-20 10:19:46 +0900

    도메인 모델링 - User에 포함되는 변수 state -> email 변경

--- com.sprint.mission.discodeit.entity/User.java
+++ com.sprint.mission.discodeit.entity/User.java
@@ -1,14 +1,14 @@
 package com.sprint.mission.discodeit.entity;
 
 public class User extends BaseEntity {
-    // 유저 이름과 상태
+    // 유저 이름과 이메일
     private String username;
-    private String state;
+    private String email;
 
-    public User(String username, String state) {
+    public User(String username, String email) {
         super(); // BaseEntity 생성자 호출
         this.username = username;
-        this.state = state;
+        this.email = email;
     }
 
     // Getter 메서드
@@ -16,14 +16,14 @@ public class User extends BaseEntity {
         return username;
     }
 
-    public String getstate() {
-        return state;
+    public String getemail() {
+        return email;
     }
 
     // 업데이트 메서드
-    public void updateUser(String username, String state) {
+    public void updateUser(String username, String email) {
         this.username = username;
-        this.state = state;
+        this.email = email;
         updateTimestamp(); // updatedAt 변경
     }
 }
