9b22f8044d1b01d8e9296020761fa93961aa9761
김민식 <u1r2r3@naver.com>
2025-02-20 11:24:25 +0900

    서비스 설계 및 구현 - 서비스 인터페이스의 구현체 작성

---
+++ com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -0,0 +1,50 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.*;
+
+public class JCFChannelService implements ChannelService {
+    // JCF를 이용하여 저장할 수 있는 필드(data)를 final로 선언
+    // Key - Value를 이용하여 저장하는 Map이용. 데이터 키 기간으로 검색할 수 있도록
+    private final Map<UUID, Channel> channels;
+
+    public JCFChannelService() {
+        this.channels = new HashMap<>();
+    }
+
+    // 채널 생성
+    @Override
+    public void createChannel(String name) {
+        Channel channel = new Channel(name);
+        channels.put(channel.getId(), channel);
+    }
+
+    // ID를 통한 채널 조회
+    @Override
+    public Channel getChannelById(UUID id) {
+        return channels.get(id);
+    }
+
+    // 모든 채널 조회 후 List 배열로 정리
+    @Override
+    public List<Channel> getAllChannels() {
+        return new ArrayList<>(channels.values());
+    }
+
+    // 채널 수정
+    @Override
+    public void updateChannel(UUID id, String name) {
+        Channel channel = channels.get(id);
+        if (channel != null) {
+            channel.updateChannel(name);
+        }
+    }
+
+    // 채널 삭제
+    @Override
+    public void deleteChannel(UUID id) {
+        channels.remove(id);
+    }
+}
---
+++ com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -0,0 +1,52 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class JCFUserService implements UserService {
+    // JCF를 이용하여 저장할 수 있는 필드(data)를 final로 선언
+    // Key - Value를 이용하여 저장하는 Map이용. 데이터 키 기간으로 검색할 수 있도록
+    private final Map<UUID, User> users;
+
+    public JCFUserService() {
+        this.users = new HashMap<>();
+    }
+
+    // 필드를 활용해 생성, 조회, 수정, 삭제 메소드 제작
+    // 유저 생성(이름, 상태)
+    @Override
+    public void createUser(String username, String email) {
+        User user = new User(username, email);
+        users.put(user.getId(), user);
+    }
+
+    // UUID 기반 유저 조회
+    @Override
+    public User getUserById(UUID id) {
+        return users.get(id);
+    }
+
+    // 모든 유저 조회
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    // UUID를 기반으로 수정
+    @Override
+    public void updateUser(UUID id, String username, String email) {
+        User user = users.get(id);
+        if (user != null) {
+            user.updateUser(username, email);
+        }
+    }
+
+    // 삭제
+    @Override
+    public void deleteUser(UUID id) {
+        users.remove(id);
+    }
+
+}
