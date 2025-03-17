9e6cb338b345bc25135a54c2817edb354aab91ed
김민식 <u1r2r3@naver.com>
2025-03-17 16:01:03 +0900

    refactor: 시간을 다루는 필드 타입을 long -> Instatnt로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -13,17 +14,17 @@ public class BaseEntity implements Serializable {
 
     // User, Channel, Message에서 공통적으로 다루는 것을 관리하는 class
     private final UUID id; // 객체를 식별하기 위한 id
-    private final long createdAt; // 객체의 생성 시간
-    private long updatedAt; // 객체의 수정 시간
+    private final Instant createdAt; // 객체의 생성 시간
+    private Instant updatedAt; // 객체의 수정 시간
 
     public BaseEntity() {
         this.id = UUID.randomUUID(); // 무작위 UUID로 생성자에서 초기화
-        this.createdAt = System.currentTimeMillis(); // 생성시간
-        this.updatedAt = System.currentTimeMillis(); // 생성된 시간으로 업데이트
+        this.createdAt = Instant.now(); // 생성 시간 저장 (UTC 기준)
+        this.updatedAt = Instant.now(); // 생성된 시간으로 업데이트
     }
 
     // updateAt 갱신 메서드
     protected void updateTimestamp() {
-        this.updatedAt = System.currentTimeMillis();
+        this.updatedAt = Instant.now();
     }
 }
