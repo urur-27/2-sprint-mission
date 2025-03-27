7e70fa76258e2646052f0debaa82f26c3da5fc7e
김민식 <u1r2r3@naver.com>
2025-03-27 10:45:19 +0900

    feat: 바이너리 파일 다운로드 API 등록
    - 특정 채널의 메시지 수신 정보 생성, 특정 채널의 메시지 수신 정보 수정, 특정 사용자의 메시지 수신 정보 조회

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,49 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto2.FileMetaResponse;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@RestController
+@RequestMapping("/api/binaryContent")
+@RequiredArgsConstructor
+public class BinaryContentController {
+    private final BinaryContentService binaryContentService;
+
+    // 단일 파일 조회
+    @RequestMapping(value = "/find", method = RequestMethod.GET)
+    public ResponseEntity<Resource> downloadFile(@RequestParam UUID binaryContentId) {
+        BinaryContent content = binaryContentService.findById(binaryContentId);
+        ByteArrayResource resource = new ByteArrayResource(content.getData());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(content.getContentType()))
+                .contentLength(content.getSize())
+                .body(resource);
+    }
+
+    // 여러 파일 조회
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ResponseEntity<List<FileMetaResponse>> downloadMultipleFiles(@RequestParam List<UUID> ids) {
+        List<FileMetaResponse> metas = ids.stream().map(id -> {
+            BinaryContent content = binaryContentService.findById(id);
+            return new FileMetaResponse(id.toString(), content.getContentType(), "/api/binaryContent/view?binaryContentId=" + id);
+        }).toList();
+
+        return ResponseEntity.ok(metas);
+    }
+}
