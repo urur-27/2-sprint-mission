66d6afd1fee15b938c399afe31b2a7a6dc787be3
김민식 <u1r2r3@naver.com>
2025-05-26 09:41:23 +0900

    refactor: dotenv를 이용한 환경변수 등록 제거

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,18 +1,11 @@
 package com.sprint.mission.discodeit;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscodeitApplication {
   public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.load();
-
-    // 환경 변수 등록
-    dotenv.entries().forEach(entry ->
-        System.setProperty(entry.getKey(), entry.getValue())
-    );
 
     SpringApplication.run(DiscodeitApplication.class, args);
   }
