7559eeb35fb87438304d4011186c7c8f3614b3b9
김민식 <u1r2r3@naver.com>
2025-07-07 11:15:54 +0900

    feat: 권한 수정 리팩토링
    - JwtSession 테이블에서 세션을 모두 삭제하여 강제 로그아웃 처리

--- src/main/java/com/sprint/mission/discodeit/repository/JwtSessionRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/JwtSessionRepository.java
@@ -2,8 +2,11 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.security.jwt.JwtSession;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JwtSessionRepository extends JpaRepository<JwtSession, Long> {
     Optional<JwtSession> findByRefreshToken(String refreshToken);
+
+    void deleteAllByUserId(UUID userId);
 }
